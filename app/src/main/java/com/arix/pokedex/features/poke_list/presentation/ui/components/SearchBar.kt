bdc3b8b6e752e967eac25c2566da6fff94922c4a
arix2000 <arix20003@gmail.com>
2022-10-07 14:19:56 +0200

    fix search bar icons color

--- app/src/main/java/com/arix/pokedex/features/poke_list/presentation/ui/components/SearchBar.kt
+++ app/src/main/java/com/arix/pokedex/features/poke_list/presentation/ui/components/SearchBar.kt
@@ -1,25 +1,20 @@
 package com.arix.pokedex.features.poke_list.presentation.ui.components
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.material.icons.rounded.Search
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.stringResource
@@ -28,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.arix.pokedex.R
 import com.arix.pokedex.theme.PokedexTheme
+import com.arix.pokedex.theme.Primary
 
 @Composable
 fun SearchBar(
@@ -39,6 +35,8 @@ fun SearchBar(
     val isPreview = LocalInspectionMode.current
     var isFilled by rememberSaveable { mutableStateOf(isPreview) }
     val focusManager = LocalFocusManager.current
+    val unfocusedColor = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+    var iconsColor: Color by remember { mutableStateOf(unfocusedColor) }
 
     BackHandler(text.isNotEmpty() && clearOnBackPress) {
         text = ""
@@ -50,7 +48,13 @@ fun SearchBar(
         singleLine = true,
         shape = CircleShape,
         label = { Text(text = "Search") },
-        leadingIcon = { Icon(imageVector = Icons.Rounded.Search, contentDescription = null) },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Rounded.Search,
+                contentDescription = null,
+                tint = iconsColor
+            )
+        },
         keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
         keyboardActions = KeyboardActions { focusManager.clearFocus() },
         onValueChange = {
@@ -60,15 +64,28 @@ fun SearchBar(
         },
         trailingIcon = {
             if (isFilled)
-                Icon(
-                    imageVector = Icons.Rounded.Clear,
-                    contentDescription = stringResource(R.string.search_clear),
-                    modifier = Modifier.clickable { text = ""; onValueChange(text) }
-                )
+                IconButton(
+                    onClick = {
+                        text = ""
+                        onValueChange(text)
+                        isFilled = false
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Clear,
+                        contentDescription = stringResource(R.string.search_clear),
+                        tint = iconsColor
+                    )
+                }
         },
         modifier = modifier
             .fillMaxWidth()
             .height(60.dp)
+            .onFocusChanged {
+                iconsColor = if (it.isFocused)
+                    Primary
+                else unfocusedColor
+            }
     )
 }
 
