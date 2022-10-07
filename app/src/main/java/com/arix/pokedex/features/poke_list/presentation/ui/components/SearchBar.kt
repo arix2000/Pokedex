2088f99f3c4127bb21d268e50c05c898ba806de5
arix2000 <arix20003@gmail.com>
2022-10-07 13:58:18 +0200

    fix clearing search text on back press in pokemon list

--- app/src/main/java/com/arix/pokedex/features/poke_list/presentation/ui/components/SearchBar.kt
+++ app/src/main/java/com/arix/pokedex/features/poke_list/presentation/ui/components/SearchBar.kt
@@ -1,17 +1,24 @@
 package com.arix.pokedex.features.poke_list.presentation.ui.components
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.material.icons.rounded.Search
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalInspectionMode
@@ -23,11 +30,21 @@ import com.arix.pokedex.R
 import com.arix.pokedex.theme.PokedexTheme
 
 @Composable
-fun SearchBar(onValueChange: (String) -> Unit, modifier: Modifier = Modifier) {
+fun SearchBar(
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    clearOnBackPress: Boolean = true
+) {
     var text by rememberSaveable { mutableStateOf("") }
     val isPreview = LocalInspectionMode.current
     var isFilled by rememberSaveable { mutableStateOf(isPreview) }
     val focusManager = LocalFocusManager.current
+
+    BackHandler(text.isNotEmpty() && clearOnBackPress) {
+        text = ""
+        onValueChange(text)
+    }
+
     OutlinedTextField(
         value = text,
         singleLine = true,
